134631bbcd0cd1cae203b75ac5d4910a1ac8d2e6
ykkim010201@gmail.com <ykkim010201@gmail.com>
2023-07-27 21:54:23 +0900

    feat: Mainscene에 db 추가

--- Assets/Scripts/Database/MainSceneDB.cs
+++ Assets/Scripts/Database/MainSceneDB.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using TMPro;
 
 using System;
@@ -9,26 +10,40 @@ using System.IO;
 using UnityEngine.Networking;
 using Mono.Data.Sqlite;
 
-public class UserDB : MonoBehaviour
+public class MainSceneDB : MonoBehaviour
 {
+
+    //************** Main Scene **************
+    //top
     public TextMeshProUGUI levelText;
     public TextMeshProUGUI coinText;
+    // bottom
+    public Slider likabilityBar;
+    public Slider cleanlinessBar;
+    public Slider depressionBar;
+    public Slider hungerBar;
 
 
+    //************** db **************
     string DBName = "test1.db";
-    int userNumU = 1;   // userNum is 1
+    int userNum_one = 1;   // userNum is 1
 
+
+    //************** table data **************
     // dog table
     int data_userNum;
     string data_dogName;
     int data_breed;
-    int data_money;
-    int data_health;
+    int data_likability;
     int data_cleanliness;
+    int data_depression;
     int data_hunger;
     int data_exp;
+    int data_money;
     int data_level;
     int data_walkDistance;
+
+
 
 
 
@@ -140,7 +155,7 @@ public class UserDB : MonoBehaviour
 
         while (dataReader.Read())
         {
-            Debug.Log($"{dataReader.GetInt32(0)}");
+            //Debug.Log($"{dataReader.GetInt32(0)}");
         }
         dataReader.Dispose();
         dataReader = null;
@@ -150,14 +165,16 @@ public class UserDB : MonoBehaviour
         dbConnection = null;
     }
 
-    // 사용자정의
+
+
+    //************** User definition functions  **************
     public void DBMainSceneInitialize()
     {
         IDbConnection dbConnection = new SqliteConnection(GetDBFilePath());
         dbConnection.Open();
         IDbCommand dbCommand=dbConnection.CreateCommand();
 
-        dbCommand.CommandText = "select * from dog where userNum=1";
+        dbCommand.CommandText = "select * from dog where userNum="+userNum_one;
         IDataReader dataReader = dbCommand.ExecuteReader();
     
         while (dataReader.Read())
@@ -168,15 +185,24 @@ public class UserDB : MonoBehaviour
             //data_userNum;
             //data_dogName
             //data_breed=dataReader.GetInt32(2);
-            data_money=dataReader.GetInt32(3);
-            coinText.text=Convert.ToString(data_money);
-            // data_health=dataReader.GetInt32(4);
-            // data_cleanliness=dataReader.GetInt32(5);
-            // data_hunger=dataReader.GetInt32(6);
-            // data_exp=dataReader.GetInt32(7);
-            data_level=dataReader.GetInt32(8);
+
+            // top
+            data_level=dataReader.GetInt32(7);
             levelText.text=Convert.ToString(data_level);
-            // data_walkDistance=dataReader.GetInt32(9);
+            data_money=dataReader.GetInt32(8);
+            coinText.text=Convert.ToString(data_money);
+
+            // bottom
+            data_likability=dataReader.GetInt32(3);
+            likabilityBar.value=data_likability;
+            data_cleanliness=dataReader.GetInt32(4);
+            cleanlinessBar.value=data_cleanliness;
+            data_depression=dataReader.GetInt32(5);
+            depressionBar.value=data_depression;
+            data_hunger=dataReader.GetInt32(6);
+            hungerBar.value=data_hunger;
+            
+            // 상시 update와 scene 들어올때마다 update 구분?
 
         }
         dataReader.Dispose();
@@ -201,6 +227,6 @@ public class UserDB : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        // 계속 update?
     }
 }
