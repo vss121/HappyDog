bb07738225e9743a65bc317be3e9feea39da35cd
ansehdals <ansehdals111@gmail.com>
2023-07-28 16:10:29 +0900

    fix: 애니메이션 최적화

--- Assets/Scripts/testShower.cs
+++ Assets/Scripts/testShower.cs
@@ -1,5 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Text;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -11,13 +13,26 @@ public class testShower : MonoBehaviour
     public GameObject Third; // 뽀송뽀송 사진(if 청결도 50이상 && 샤워완료)=> 샤워후 
     public GameObject Fourth; // 지저분한 사진(청결도 50미만)
     public GameObject Animobj;
+    public GameObject Waterobj;
+    public Button WaterBtn;
     public int score, count;
     private void Start()
     {
-        First.SetActive(true);
-        Second.SetActive(false);
-        Third.SetActive(false);
-        Fourth.SetActive(false);
+        WaterBtn = Waterobj.GetComponent<Button>();
+        if (slide.value >= 50)
+        {
+            First.SetActive(true);
+            Second.SetActive(false);
+            Third.SetActive(false);
+            Fourth.SetActive(false);
+        }
+        else
+        {
+            First.SetActive(false);
+            Second.SetActive(false);
+            Third.SetActive(false);
+            Fourth.SetActive(true);
+        }
         count = 0;
         Animobj = GameObject.Find("BackGroundCanvus").transform.Find("Shower").gameObject;
         Animobj.SetActive(false);
@@ -48,7 +63,7 @@ public class testShower : MonoBehaviour
     }
     public void Watering()
     {
-        StartCoroutine(mulbangowl());
+        StartCoroutine(mulbangowl(3.0f));
     }
     public void SettingImg() // 샤워중
     {
@@ -60,12 +75,18 @@ public class testShower : MonoBehaviour
             Fourth.SetActive(false);
         }
     }
-    IEnumerator mulbangowl()
+    IEnumerator mulbangowl(float coolTime)
     {
+        WaterBtn.enabled = false;
         Animobj.SetActive(true);
-        yield return new WaitForSeconds(3.0f);
-        Animobj.SetActive(false);
+        while (coolTime > 1.0f)
+        {
+            coolTime -= Time.deltaTime;
+            Debug.Log(coolTime);
+            yield return new WaitForFixedUpdate();
+        }
         CheckValue();
+        Animobj.SetActive(false);
     }
     public void CheckValue()
     {
@@ -84,5 +105,6 @@ public class testShower : MonoBehaviour
             Fourth.SetActive(false);
             count = 0;
         }
+        WaterBtn.enabled = true;
     }
 }
