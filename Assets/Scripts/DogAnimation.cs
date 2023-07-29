d959405167ee0d77424a781043112c9386c1a293
sanghee01 <sang6299@naver.com>
2023-07-29 22:04:47 +0900

    fix: build 에러 해결

--- Assets/Scripts/DogAnimation.cs
+++ Assets/Scripts/DogAnimation.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using UnityEngine.EventSystems;
 using UnityEngine;
 using Unity.VisualScripting;
-using Unity.UI;
 using UnityEngine.UI;
 
 public class DogAnimation : MonoBehaviour, IPointerClickHandler
